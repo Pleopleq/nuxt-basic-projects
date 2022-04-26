f399293f799bc8ef27d727ebc4ab6a42ebffe80f
Felix Anducho <felix.anducho74@gmail.com>
2022-04-25 22:47:26 -0500

    Add a simple nuxt app to check it out

---
+++ nuxt-app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <TheHeader></TheHeader>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
---
+++ nuxt-app/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Checking out Nuxt 3</h1>
+    <p>{{ works }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      works: "works yay",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  margin: 0;
+}
+div {
+  background-color: #05bd70;
+}
+</style>
